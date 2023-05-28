ef9547175c71c416d24500ec674a5210fb54d72a
nandhu-00 <nandhakumar834427@gmail.com>
2023-05-28 16:13:27 +0530

    inline css in jsx
    28/05/23

---
+++ webdev/src/inlinecss/inlinecss.js
@@ -0,0 +1,15 @@
+const styles = {
+    color: 'red',
+    fontSize: '16px',
+    fontWeight: 'bold'
+  };
+
+  const MyComponent = () => {
+    return (
+      <div style={styles}>
+        This is a inline styled div.
+      </div>
+    );
+  };
+
+export default MyComponent;
